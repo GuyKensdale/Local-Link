6ccbfda743aaea4b59170cc8cd290f12e4e3fedb
GuyKensdale <126467015+GuyKensdale@users.noreply.github.com>
2023-06-13 09:47:58 +0100

    task1-2

--- Screens/SignIn.tsx
+++ Screens/SignIn.tsx
@@ -1,9 +1,48 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Button, Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
 
-export default function LostFound({ navigation }: any) {
+import firestore from "@react-native-firebase/firestore";
+
+const SignInScreen: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = async () => {
+    try {
+      const response = await auth().signInWithEmailAndPassword(email, password);
+      const user = response.user;
+
+      // Save user data to the Firestore database
+      await firestore().collection("users").doc(user.uid).set({
+        email: user.email,
+        // Add more user data fields as needed
+      });
+
+      console.log("User signed in:", user);
+    } catch (error: any) {
+      const errorMessage = (error as { message: string }).message;
+      Alert.alert("Error", errorMessage);
+    }
+  };
+
   return (
-    <>
-      <Text>Sign-in</Text>
-    </>
+    <View>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Sign In" onPress={handleSignIn} />
+    </View>
   );
-}
+};
+
+export default SignInScreen;
