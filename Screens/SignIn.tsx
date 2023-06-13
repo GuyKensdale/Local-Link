ca86405d05fbdffd5c52d27b5e911220e0c995d2
GuyKensdale <126467015+GuyKensdale@users.noreply.github.com>
2023-06-13 10:36:31 +0100

    siginfrom branch

--- Screens/SignIn.tsx
+++ Screens/SignIn.tsx
@@ -1,48 +1,132 @@
-import React, { useState } from "react";
-import { View, TextInput, Button, Alert } from "react-native";
-import auth from "@react-native-firebase/auth";
+import React from "react";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+} from "react-native";
+import { StackScreenProps } from "@react-navigation/stack";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-import firestore from "@react-native-firebase/firestore";
+const auth = getAuth();
 
-const SignInScreen: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+type SignInScreenProps = StackScreenProps<RootStackParamList, "SignIn">;
 
-  const handleSignIn = async () => {
-    try {
-      const response = await auth().signInWithEmailAndPassword(email, password);
-      const user = response.user;
+function SignInScreen({ navigation }: SignInScreenProps) {
+  const [value, setValue] = React.useState({
+    email: "",
+    password: "",
+    error: "",
+  });
 
-      // Save user data to the Firestore database
-      await firestore().collection("users").doc(user.uid).set({
-        email: user.email,
-        // Add more user data fields as needed
+  async function signIn() {
+    if (value.email === "" || value.password === "") {
+      setValue({
+        ...value,
+        error: "Email and password are mandatory.",
       });
-
-      console.log("User signed in:", user);
-    } catch (error: any) {
-      const errorMessage = (error as { message: string }).message;
-      Alert.alert("Error", errorMessage);
+      return;
     }
-  };
+
+    try {
+      await signInWithEmailAndPassword(auth, value.email, value.password);
+    } catch (error) {
+      setValue({
+        ...value,
+        error: error.message,
+      });
+    }
+  }
 
   return (
-    <View>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Sign In" onPress={handleSignIn} />
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Sign In</Text>
+
+        <View style={styles.inputContainer}>
+          <View style={styles.inputWrapper}>
+            <Icon style={styles.icon} name="email" size={18} color="gray" />
+            <TextInput
+              placeholder="Email"
+              value={value.email}
+              style={styles.input}
+              onChangeText={(text) => setValue({ ...value, email: text })}
+            />
+          </View>
+
+          <View style={styles.inputWrapper}>
+            <Icon style={styles.icon} name="lock" size={18} color="gray" />
+            <TextInput
+              placeholder="Password"
+              style={styles.input}
+              onChangeText={(text) => setValue({ ...value, password: text })}
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+
+        <Pressable style={styles.button} onPress={signIn}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </Pressable>
+
+        <Text style={styles.signupText}>
+          Don't Have an account?{" "}
+          <Text
+            style={styles.signupLink}
+            onPress={() => navigation.navigate("Sign Up")}
+          >
+            Sign Up
+          </Text>
+        </Text>
+      </View>
     </View>
   );
-};
+}
 
 export default SignInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    width: "80%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  inputWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginBottom: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    paddingVertical: 8,
+  },
+  button: {
+    backgroundColor: "#000",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+});
