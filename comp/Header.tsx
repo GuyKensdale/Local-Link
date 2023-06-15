db571c701ea97ccf6adab16c452d15fc10f9a7cb
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-06-15 14:41:15 +0100

    Add user to header

--- comp/Header.tsx
+++ comp/Header.tsx
@@ -1,7 +1,34 @@
+import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet, Button, Image } from "react-native";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  doc,
+  getDocs,
+} from "firebase/firestore";
+import { db } from "../config/firebase";
 import colours from "../constants/colours";
 
+const noPictureUser = require("../assets/icon-user.png");
+
 export default function Header() {
+  const [username, setUsername] = useState("");
+  const [userIcon, setUserIcon] = useState(noPictureUser);
+
+  useEffect(() => {
+    const q = query(collection(db, "Users"));
+    const usersQuery = onSnapshot(q, (querySnapshot) => {
+      let usersArr: any[] = [];
+      querySnapshot.forEach((doc) => usersArr.push(doc.data()));
+      setUsername(usersArr[0].username);
+      return () => usersQuery();
+    });
+  }, []);
+  useEffect(() => {
+    console.log(username);
+  }, [username]);
   return (
     <View style={styles.container}>
       <Image
@@ -9,6 +36,14 @@ export default function Header() {
         source={require("../assets/logo-local-link.png")}
         resizeMode="contain"
       ></Image>
+      <View style={styles.user}>
+        {!username ? null : (
+          <>
+            <Text style={styles.user}>{username}!</Text>
+            {/* <Image source={userIcon} style={styles.userIcon}></Image> */}
+          </>
+        )}
+      </View>
     </View>
   );
 }
@@ -28,4 +63,12 @@ const styles = StyleSheet.create({
     height: 70,
     marginTop: -18,
   },
+  user: { alignSelf: "flex-end" },
+
+  userIcon: {},
+  //   user: {
+  //     flexDirection: "row",
+  //     alignItems: "center",
+  //     justifyContent: "center",
+  //   }
 });
